b2a8c4ee2eafb07095f984fded3e54e229829be6
zhaochaochao <zhaochaochao@corp.netease.com>
2021-11-22 20:51:48 +0800

    Add page rank

--- spark-examples/src/main/scala/CalcMean.scala
+++ spark-examples/src/main/scala/CalcMean.scala
@@ -1,7 +1,7 @@
 package org.spark_examples
 
 import org.apache.hadoop.mapred.lib.BinaryPartitioner
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 
 object CalcMean {
   def main(args: Array[String]): Unit = {
@@ -23,10 +23,11 @@ object CalcMean {
     // words.collectAsMap().map(println(_))
 
     val result = words.combineByKey(
-      (v) => (v, 1), // create combiner，创建key的累加器的初始值，每个分区key首次出现时调用
-      (acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1), // merge value，分区中key已经存在时，将新的值累加到累加器中
+      (v: Int) => (v, 1), // create combiner，创建key的累加器的初始值，每个分区key首次出现时调用
+      (acc: (Int, Int), v: Int) => (acc._1 + v, acc._2 + 1), // merge value，分区中key已经存在时，将新的值累加到累加器中
       (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2), // merge combiners，将两个分区的同一个key的累加器进行累加
-    ).map { case (key, value) => (key, value._1 / value._2.toFloat) }
+      new HashPartitioner(2) // 指定分区方式
+    ).map({ case (key, value) => (key, value._1 / value._2.toFloat) })
     result.collectAsMap().map(println(_))
 
     // 统计出来的单词总数保存到文本文件
---
+++ spark-examples/src/main/scala/PageRank.scala
@@ -0,0 +1,35 @@
+package org.spark_examples
+
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+
+object PageRank {
+  def main(args: Array[String]): Unit = {
+    println("<<<<<<<<" + args.mkString("Array(", ", ", ")"))
+    val input_file = args(0)
+    val output_dir = args(1)
+    // 创建spark context
+    val conf = new SparkConf().setAppName("pageRank").setMaster("spark://zhaocc-Lenovo-Legion-R7000P2021H:7077") // 根据实际设置spark集群的master
+    val sc = new SparkContext(conf)
+
+    var links = sc.textFile(input_file)
+      .map(line => {
+        val arr = line.split(" ")
+        (arr(0), arr.drop(0))
+      }).partitionBy(new HashPartitioner(2)) // 提前设置分区，后面操作会使用该分区方式
+      .persist() // RDD缓存到memory
+
+    var ranks = links.mapValues(v => 1.0) // 每个链接初始rank值
+
+    // 执行10次page rank迭代
+    for (i <- 0 until 10) {
+      // 每个链接对附近的链接的贡献值为rank(p) / numNeighbors(p)
+      // 计算每个链接收到的贡献值
+      val contributions = links.join(ranks).flatMap {
+        case (pageId, (links, rank)) => links.map(dest => (dest, rank / links.size))
+      }
+      // 计算每个链接的rank值
+      ranks = contributions.reduceByKey((x, y) => x + y).mapValues(v => 0.15 + 0.85 * v)
+    }
+    ranks.saveAsTextFile(output_dir + "pages")
+  }
+}
